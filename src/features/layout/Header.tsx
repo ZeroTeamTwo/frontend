b499a1f8eb607310104db260fd04810897071144
suhong99 <topic828@naver.com>
2025-04-18 13:22:20 +0900

    폰트반영

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
 	return (
 		<div className="flex justify-between items-center fixed top-0 w-full max-w-max mx-auto p-4 bg-red-90">
-			<div>로고</div>
-			<div>네브바</div>
-			<div>검색</div>
+			<div className="font-medium">로고</div>
+			<div className="font-bold">네브바</div>
+			<div className="font-regular">검색</div>
 			<div>링크</div>
 		</div>
 	);
