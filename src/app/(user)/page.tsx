c3ed8a601606c62ebd9178843e7d7859a9ee642b
suhong99 <topic828@naver.com>
2025-04-26 21:12:23 +0900

    홈페이지 UI작업

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,17 +1,18 @@
+import Content from '@/features/home/Content';
+import ContentHeader from '@/features/home/ContentHeader';
+
 export default function Home() {
 	return (
-		<div className="flex items-center justify-center min-h-screen flex-col gap-0.5">
-			<div className="items-center flex-col text-2xl">하이보다 커야함</div>
-			<div className="typo-display1">디스플레이1</div>
-			<div className="typo-display2">디스플레이</div>
-			<div className="typo-title1">타이틀</div>
-			<div className="typo-body2-normal">바디노말</div>
-			<div className="typo-body2-reading">바디리딩</div>
-			<div className="typo-label2">라벨</div>
-			<div className="typo-caption1">캡션</div>
-			<div className="typo-label1-reading">캡션</div>
-			<div className="typo-headline2">테스트</div>
-			<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-		</div>
+		<>
+			<div className="w-full h-[250px] flex items-center justify-center  max-w-maxw bg-primary-main-strong">배너 </div>
+			<div className="w-full flex flex-col flex-1 gap-12 items-center px-9 py-12 bg-bg-gray ">
+				<Content>
+					<ContentHeader title="누구님이 관심있는" keyword_list={['정치', '피그마', '회의']} link="/issue" />
+				</Content>
+				<Content>
+					<ContentHeader title="당신을 위한 법안" isLoginRequired link="/issue" />
+				</Content>
+			</div>
+		</>
 	);
 }
--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<div className="flex justify-between items-center fixed top-0 w-full max-w-max mx-auto p-4 bg-red-90">
-			<div className="font-medium">로고</div>
-			<div className="font-bold">네브바</div>
-			<div className="">유저프로필</div>
+		<div className="w-full px-5 mx-auto flex justify-center">
+			<div className="flex justify-between items-center sticky top-0 w-full max-w-maxw bg-red-90 h-16">
+				<div className="font-medium">로고</div>
+				<div className="font-bold">네브바</div>
+				<div className="">유저프로필</div>
+			</div>
 		</div>
 	);
 };
