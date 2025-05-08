4d67ca468f8225dd955a719b80968627e4ca1c94
suhong99 <topic828@naver.com>
2025-05-08 22:40:05 +0900

    suspense걸기

--- src/features/bill/QueryBar.tsx
+++ src/features/bill/QueryBar.tsx
@@ -6,11 +6,11 @@ import CategoryFIlter from './CategoryFIlter';
 const QueryBar = () => {
 	return (
 		<section className="w-full flex justify-between items-center gap-5 max-w-maxw">
-			<div className="flex gap-1.5 desktop:gap-3.5 ">
-				<OrderFilter />
-				<CategoryFIlter />
-			</div>
 			<Suspense>
+				<div className="flex gap-1.5 desktop:gap-3.5 ">
+					<OrderFilter />
+					<CategoryFIlter />
+				</div>
 				<SearchBar />
 			</Suspense>
 		</section>
