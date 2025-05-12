67c49fe727ec7f0fa71e4eb362175934f5bdff89
suhong99 <topic828@naver.com>
2025-05-12 12:23:10 +0900

    햄버거메뉴 만들기

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,16 +2,16 @@ import Header from '@/features/layout/Header';
 
 export default function Layout({
 	children,
-	modal,
+	overlay,
 }: Readonly<{
 	children: React.ReactNode;
-	modal: React.ReactNode;
+	overlay: React.ReactNode;
 }>) {
 	return (
 		<>
 			<Header />
 			<main className="flex flex-col flex-1 items-center w-full">{children}</main>
-			{modal}
+			{overlay}
 		</>
 	);
 }
