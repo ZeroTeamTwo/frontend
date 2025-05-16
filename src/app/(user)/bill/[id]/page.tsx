c1d7cee0845d5cc2ad578e60277624a736660baf
suhong99 <topic828@naver.com>
2025-05-16 18:17:32 +0900

    플로팅반응

--- src/app/(user)/bill/[id]/page.tsx
+++ src/app/(user)/bill/[id]/page.tsx
@@ -1,5 +1,12 @@
+import { getBillDetail } from '@/features/bill-detail/api';
+import FloatingBtn from '@/features/bill-detail/FloatingReaction';
+
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
 	const { id } = await params;
-
-	return <>My Post: {id}</>;
+	const data = getBillDetail(id);
+	return (
+		<div className="flex flex-col items-center p-5 pb-[100px]  desktop:pt-12 desktop:pb-[160px]">
+			<FloatingBtn reactions={data.reactions} myReaction={data.myReaction} />
+		</div>
+	);
 }
