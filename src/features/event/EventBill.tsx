4794878322ac358068bd06a3a14486656544d7fd
suhong99 <topic828@naver.com>
2025-05-26 19:43:23 +0900

    법안관련 링크 및 모달창 생성

--- src/features/event/EventBill.tsx
+++ src/features/event/EventBill.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { EVENT_DETAIL_DATA, EventPerson } from './const/data';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const EventBill = ({ name }: { name: EventPerson }) => {
 	const bill = EVENT_DETAIL_DATA[name].bill;
@@ -12,12 +13,16 @@ const EventBill = ({ name }: { name: EventPerson }) => {
 			</h2>
 			<div className="flex flex-col gap-6 desktop:gap-10">
 				{bill.map(({ id, category, title }) => (
-					<div key={id} className="flex  w-full items-center px-5 py-4 gap-4 rounded-[12px] desktop:h-[56px] bg-[#FBFAFF]">
-						<span className="typo-label1-normal text-label-normal w-[65px] text-center desktop:typo-headline1 desktop:w-[80px] ">{category}</span>
-						<div className="w-[3px] h-[16px] bg-[#E1E2E4]" />
-						<span className="typo-label1-normal desktop:typo-headline1 break-words">{title}</span>
+					<Link
+						href={`/modal-event-bill/${name}/${id}`}
+						key={id}
+						className="flex  w-full items-center px-5 py-4 gap-4 rounded-[12px] desktop:h-[56px] bg-[#FBFAFF]"
+					>
+						<span className="typo-label1-normal text-label-normal w-[64px] text-center desktop:typo-headline1 desktop:w-[80px] ">{category}</span>
+						<div className="w-[2.5px] h-[16px] bg-[#E1E2E4]" />
+						<span className="flex-1 typo-label1-normal desktop:typo-headline1 break-words">{title}</span>
 						<Image src={'/svgs/right.svg'} alt="추가 내용있음" width={10} height={18} className="text-[#E1E2E4]" />
-					</div>
+					</Link>
 				))}
 			</div>
 		</section>
