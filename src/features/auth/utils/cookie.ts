4ce5abf88625755198b11cf0b523b85620694288
suhong99 <topic828@naver.com>
2025-06-08 06:55:20 +0900

    로그아웃 및 리다이렉트 로직 구현

--- src/features/auth/utils/cookie.ts
+++ src/features/auth/utils/cookie.ts
@@ -1,3 +1,6 @@
+'use server';
+
+import { COOKIE_NAME } from '@/shared/const/cookie';
 import { cookies } from 'next/headers';
 
 type AuthResult = {
@@ -10,7 +13,9 @@ type AuthResult = {
 export const storeAuth = async (result: AuthResult) => {
 	const cookieStore = await cookies();
 
-	cookieStore.set('accesstoken', result.accessToken, {
+	const { access, refresh, userId, nickname } = COOKIE_NAME.auth;
+
+	cookieStore.set(access, result.accessToken, {
 		httpOnly: true,
 		secure: true,
 		sameSite: 'lax',
@@ -18,23 +23,42 @@ export const storeAuth = async (result: AuthResult) => {
 		maxAge: 60 * 60, // 1시간
 	});
 
-	cookieStore.set('accesstoken', result.refreshToken, {
+	cookieStore.set(refresh, result.refreshToken, {
 		httpOnly: true,
 		secure: true,
 		sameSite: 'lax',
+
 		path: '/',
-		maxAge: 60 * 60 * 24 * 7, // 7일
+		maxAge: 60 * 60 * 24 * 7,
 	});
 
-	cookieStore.set('userid', result.userid.toString(), {
+	cookieStore.set(userId, result.userid.toString(), {
 		httpOnly: false,
 		path: '/',
 		maxAge: 60 * 60 * 24 * 7,
 	});
 
-	cookieStore.set('nickname', result.nickname ?? '', {
+	cookieStore.set(nickname, result.nickname ?? '', {
 		httpOnly: false,
 		path: '/',
 		maxAge: 60 * 60 * 24 * 7,
 	});
+};
+
+// 로그아웃 로직 (쿠키 삭제)
+export const clearAuth = async () => {
+	const cookieStore = await cookies();
+
+	cookieStore.delete('accesstoken');
+	cookieStore.delete('refreshtoken');
+	cookieStore.delete('userid');
+	cookieStore.delete('nickname');
+};
+
+// 로그인인지
+export const isLoggedIn = async () => {
+	const cookieStore = await cookies();
+	const hasAccessToken = cookieStore.has('accesstoken');
+
+	return hasAccessToken;
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,36 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { COOKIE_NAME } from './shared/const/cookie';
 
 export async function middleware(req: NextRequest) {
 	const { pathname } = req.nextUrl;
 	const cookieStore = await cookies();
 
-	const accessToken = cookieStore.get('accesstoken');
-	const nickname = cookieStore.get('nickname');
+	// 오타 방지용 상수값 관리
+	const { access, refresh, userId, nickname } = COOKIE_NAME.auth;
+	const protectedPaths = ['/onboarding', '/mypage'];
 
-	console.log('Middleware Access Token:', accessToken);
-	if (!accessToken && pathname.startsWith('/onboarding')) {
+	const hasToken = cookieStore.has(access);
+	const nickValue = cookieStore.get(nickname)?.value;
+
+	// 닉네임 없이 onboarding페이지 이탈시 토큰 제거 (로그아웃)
+	if (hasToken && nickValue === '' && !(pathname === '/onboarding')) {
+		cookieStore.delete(access);
+		cookieStore.delete(refresh);
+		cookieStore.delete(userId);
+		cookieStore.delete(nickname);
+
+		if (pathname !== '/onboarding') {
+			return NextResponse.redirect(new URL('/', req.url));
+		}
+	}
+
+	// 토큰없을 경우 특정 페이지 진입 불가
+	if (!hasToken && protectedPaths.includes(pathname)) {
 		return NextResponse.redirect(new URL('/', req.url));
 	}
-	if (accessToken && !nickname?.value && !pathname.startsWith('/onboarding')) {
-		return NextResponse.redirect(new URL('/onboarding', req.url));
-	}
+
 	return NextResponse.next();
 }
 
