006276d6251aa07d9fbcab94a3b6556a6987f748
suhong99 <topic828@naver.com>
2025-06-17 20:27:06 +0900

    fix:image관련 lint준수

--- src/app/(user)/not-found.tsx
+++ src/app/(user)/not-found.tsx
@@ -5,18 +5,8 @@ import { SolidBtn } from '@/shared/components/SolidBtn';
 export default function NotFound() {
 	return (
 		<div className="mx-auto flex flex-col items-center px-6 py-8 gap-6 desktop:py-12">
-			<div className="w-[185px] desktop:w-[300px]">
-				<Image
-					src="/images/oops.png"
-					alt="not-found"
-					width={300}
-					height={415}
-					sizes="100vw"
-					style={{
-						width: '100%',
-						height: 'auto',
-					}}
-				/>
+			<div className="relative w-[185px] desktop:w-[300px] aspect-[300/415]">
+				<Image src="/images/oops.png" alt="not-found" fill sizes="(min-width: 960px) 300px, 185px" priority className="object-contain" />
 			</div>
 
 			<div className="flex flex-col items-center gap-3">
