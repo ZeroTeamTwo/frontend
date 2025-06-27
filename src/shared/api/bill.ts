21906bfbd116525df2f7f703a164ce0c759728ef
suhong99 <topic828@naver.com>
2025-06-27 21:20:04 +0900

    feat:북마크기능 추가

--- src/shared/api/bill.ts
+++ src/shared/api/bill.ts
@@ -1,26 +1,28 @@
 'use server';
 
-import { redirect } from 'next/navigation';
-import { NeedLoginError, RefreshTokenError } from '../const/error';
+import { cookies } from 'next/headers';
 import { tokenFetcher } from './fetcher';
-import { clearAuth } from '@/features/auth/utils/cookie';
+import { COOKIE_NAME } from '../const/cookie';
+import { NeedLoginError, RefreshTokenError } from '../const/error';
 
 export const toggleScrapped = async (id: string) => {
 	try {
+		const cookieStore = await cookies();
+		if (!cookieStore.get(COOKIE_NAME.auth.access)?.value) {
+			return { status: 'Relogin' };
+		}
+
 		const response = await tokenFetcher<boolean>(`/api/bills/${id}/scraps/toggle`, { method: 'POST' });
 
-		// 최종 북마크 상태 (boolean)
-		return response.result;
+		return {
+			status: 'SUCCESS',
+			result: response.result,
+		};
 	} catch (err) {
-		if (err instanceof NeedLoginError) {
-			redirect('/modal-login');
+		if (err instanceof NeedLoginError || err instanceof RefreshTokenError) {
+			return { status: 'Relogin' };
 		}
 
-		if (err instanceof RefreshTokenError) {
-			await clearAuth();
-			redirect('/modal-link');
-		}
-
-		throw new Error('서버에 문제가 생겼습니다.');
+		return { status: 'ERROR', message: (err as Error).message };
 	}
 };
