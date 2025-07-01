91ecbce49603999fd0ffac287ec5ff9fbff88a87
suhong99 <topic828@naver.com>
2025-07-02 01:52:55 +0900

    feat:mypage 내 댓글 구현

--- src/features/mypage/MyOpinion.tsx
+++ src/features/mypage/MyOpinion.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+'use client';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getMyReactions } from './api/server';
+import { useEffect, useRef } from 'react';
+import { useInView } from 'framer-motion';
 
 const MyOpinion = () => {
-	return <div>MyOpinion</div>;
+	const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
+		queryKey: ['reactions'],
+		queryFn: ({ pageParam }) => getMyReactions({ page: pageParam }),
+		initialPageParam: 1,
+		getNextPageParam: (lastPage) => {
+			if (!lastPage.result.last) {
+				return lastPage.result.pageNumber + 1;
+			}
+			return undefined;
+		},
+	});
+
+	const sensorRef = useRef(null);
+	const isInView = useInView(sensorRef);
+
+	useEffect(() => {
+		if (!isFetching && hasNextPage && isInView) {
+			fetchNextPage();
+		}
+	}, [fetchNextPage, hasNextPage, isFetching, isInView]);
+
+	console.log(data);
+	return (
+		<div className="grid grid-cols-1 ">
+			{data?.pages.map(({ result }) => result.content.map((reaction) => <div key={reaction.billId} className="bg-accent-bg-blue" />))}
+			<div ref={sensorRef} className="bg-accent-bg-red w-full h-10" />
+		</div>
+	);
 };
 
 export default MyOpinion;
