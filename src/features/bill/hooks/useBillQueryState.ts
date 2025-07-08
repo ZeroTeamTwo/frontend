95c0521ea21af49b4b1e47a08cfe57a8a0177ef1
suhong99 <topic828@naver.com>
2025-07-08 19:31:12 +0900

    feat:법안페이지 무한스크롤 구현

--- src/features/bill/hooks/useBillQueryState.ts
+++ src/features/bill/hooks/useBillQueryState.ts
@@ -1,11 +1,12 @@
+import { Keyword } from '@/shared/const/committee';
 import { useSearchParams } from 'next/navigation';
 
 export const useBillQueryState = () => {
 	const searchParams = useSearchParams();
 
-	const order = searchParams.get('order') ?? '최신순';
+	const order = searchParams.get('order') === '조회순' ? 'viewCount' : 'proposeDate';
 	const keywordsParam = searchParams.get('keywords') ?? '';
-	const keywords = keywordsParam ? keywordsParam.split(',') : [];
+	const keywords = keywordsParam ? (keywordsParam.split(',') as Keyword[]) : [];
 
 	return { order, keywords };
 };
