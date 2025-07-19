9b1b62fcb02934e2e45e8afdf86f56c2c5800cfa
suhong99 <topic828@naver.com>
2025-07-19 14:30:52 +0900

    fix: 모달창 열때 scroll옵션 추가하여 화면 상단으로 이동하지 않게 함

--- src/shared/hooks/useHandleError.ts
+++ src/shared/hooks/useHandleError.ts
@@ -6,10 +6,10 @@ export const useHandleError = () => {
 
 	const handleErrorByName = (err: Error, actionName: string) => {
 		if (err.name === 'NeedLoginError') {
-			router.push(MODAL_PATH.login);
+			router.push(MODAL_PATH.login, { scroll: false });
 		} else if (err.name === 'RefreshTokenError') {
 			alert('세션이 만료되었습니다. 다시 로그인해주세요.');
-			router.push(MODAL_PATH.login);
+			router.push(MODAL_PATH.login, { scroll: false });
 		} else {
 			alert(`${actionName}에 실패했습니다. 다시 시도해주세요.`);
 		}
