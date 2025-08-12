5426c277aabee3f9cc4ba490d124f428a62387ab
suhong99 <topic828@naver.com>
2025-08-12 19:56:30 +0900

    bug:위원회없는 경우에 대한 예외처리

--- src/features/bill-detail/DetailContent.tsx
+++ src/features/bill-detail/DetailContent.tsx
@@ -5,7 +5,7 @@ const DetailContent = ({ detail }: { detail: string }) => {
 		<section className="flex flex-col w-full px-1 py-5 gap-3 desktop:px-9 desktop:py-8">
 			<h3 className="typo-heading2 font-bold text-label-normal desktop:typo-heading1">제안 이유 및 주요 내용</h3>
 			<p className="whitespace-pre-wrap typo-body2-normal font-regular text-label-normal desktop:typo-body1-normal">
-				{normalizeTextStructure(detail)}{' '}
+				{normalizeTextStructure(detail)}
 			</p>
 		</section>
 	);
